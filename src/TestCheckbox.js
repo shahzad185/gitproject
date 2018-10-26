944c14e3573cb2ef25bc60646dcfa250cabea785
Muhammed Shehzad <mshehzad@xppower.com>
2018-10-26 09:44:36 +0100

    Checkbox added

---
+++ src/TestCheckbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TestCheckbox = (props) => {
+
+    return <input type="checkbox" name="Movie category" checked="true" />
+
+
+
+
+}
+
+export default TestCheckbox;
