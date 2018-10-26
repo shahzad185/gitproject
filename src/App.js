3cdaadaa2d8f6800629a8cdf9f1202ce437688d0
Muhammed Shehzad <mshehzad@xppower.com>
2018-10-26 09:45:18 +0100

    App modified for TestCheckbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestCheckbox from './TestCheckbox';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        I am simple DIV :)
+      <div>
+        <div >
+          I am simple DIV :)
+      </div>
+        <TestCheckbox />
       </div>
     );
   }
